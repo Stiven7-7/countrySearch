7d474b246ceff0bb510e64cc3e3c2c3c3979dcef
Brayan Pinzon <58951748+Stiven7-7@users.noreply.github.com>
2024-08-10 15:53:23 -0500

    Configurando routing y agregar pages shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutinModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutinModule,
     BrowserModule,
     SharedModule
   ],
