5a28d15af063374db65e85de1622c85b15b7d399
Brayan Pinzon <58951748+Stiven7-7@users.noreply.github.com>
2024-08-11 17:06:56 -0500

    Mostrar la informacion de los paises mas detallada

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/countries';
 
 @Component({
   selector: 'app-country-page',
@@ -6,6 +10,27 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  country?: Country;
+
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private countriesServices: CountriesService,
+    private router: Router
+  ){
+
+  }
+
+  ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.countriesServices.searchContryAlphaCode( id ) )
+      )
+      .subscribe( country => {
+       if (!country) return this.router.navigateByUrl('');
+       return this.country = country;
+      });
+  }
 
 }
