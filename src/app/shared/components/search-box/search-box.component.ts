225589b059b39f9aa424be97f4a0927c9499eed0
Brayan Pinzon <58951748+Stiven7-7@users.noreply.github.com>
2024-09-04 20:17:22 -0500

    Desarrollo -  busqueda mejorada con debounce

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -7,12 +8,36 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   ]
 })
 
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy {
+
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
 
   @Input() placeholder: string = '';
   @Output() onValue = new EventEmitter<string>();
+  @Output() onDebounce  = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncerSubscription = this.debouncer
+      .pipe(
+        debounceTime(300)
+      )
+      .subscribe( value =>{
+        this.onDebounce.emit( value );
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();
+  }
+
 
   emitValue(value: string){
     this.onValue.emit(value);
+  }
+
+  onKeyPress( searchTerm: string ){
+    this.debouncer.next( searchTerm );
+
   }
 }
